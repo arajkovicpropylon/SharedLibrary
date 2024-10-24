dc65b95155685324877b0d13425caf3ab53851cc
arajkovicpropylon <aleksandar.rajkovic@propylon.com>
2024-10-24 12:30:09 +0200

    works

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface MyDivProps {
+    name: string;
+}
+
+const MyDiv: React.FC<MyDivProps> = ({name}) => {
+    return <div> Hello {name} </div>;
+};
+
+export default MyDiv;
